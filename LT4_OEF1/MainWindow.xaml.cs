f974438c21d2354d12be466ae56b3cdde99ad493
Guillaume Oudouoir <guillaume.oudouoir@gmail.com>
2022-01-29 16:13:17 +0100

    done i think

--- LT4_OEF1/MainWindow.xaml.cs
+++ LT4_OEF1/MainWindow.xaml.cs
@@ -20,9 +20,126 @@ namespace LT4_OEF1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Medewerker> medewerkerList = new List<Medewerker>();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            cmbGeslacht.Items.Add("m");//voegt geslacht toe aan de combobox
+            cmbGeslacht.Items.Add("v");
+            cmbGeslacht.Items.Add("x");
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (medewerkerList.Count == 25)//max 25 list items
+                {
+                    MessageBox.Show("Je hebt het maximum aantal werknemers bereikt.");
+                }
+                else
+                {//voegt alles toe aan de list en cleart dan de ingevulde gegevens
+
+                    medewerkerList.Add(new Medewerker(txbVoornaam.Text, txbFamilienaam.Text, Convert.ToDouble(txbMaandwedde.Text), txbStatuut.Text, Convert.ToChar(cmbGeslacht.SelectedItem)));
+                    lsbLeden.Items.Add($"{txbVoornaam.Text}, {txbFamilienaam.Text}");
+                    txbFamilienaam.Clear();
+                    txbMaandwedde.Clear();
+                    txbOpslag.Clear();
+                    txbStatuut.Clear();
+                    cmbGeslacht.SelectedIndex = -1;
+                    txbVoornaam.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+                bool isDouble = Double.TryParse(txbMaandwedde.Text, out double temp);
+                if (isDouble) { MessageBox.Show("Check je ingevulde waarden en probeer opnieuw"); }
+                else
+                {
+                    MessageBox.Show("Je hebt geen juiste waarde ingegeven als loon herzie deze info.");
+                }
+
+            }
+        }
+
+
+        private void lsbLeden_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (lsbLeden.SelectedIndex != -1)//voegt de info toe aan de label
+            {
+                int index = lsbLeden.SelectedIndex;
+                lblLidRes.Content = $"Voornaam: {medewerkerList[index].voornaam}\nFamilienaam: {medewerkerList[index].familienaam}\nMaandwedde: €{medewerkerList[index].maandwedde}\nStatuut: {medewerkerList[index].statuut}\nGeslacht: {medewerkerList[index].geslacht}";
+            }
+        }
+
+        private void btnPlus_Click(object sender, RoutedEventArgs e)//geeft de opslag aan de geselecteerde persoon
+        {
+            if (lsbLeden.SelectedIndex != -1)
+            {
+                try
+                {
+                    int index = lsbLeden.SelectedIndex;
+                    medewerkerList[index].opslag(Convert.ToDouble(txbOpslag.Text));
+                    lblLidRes.Content = $"Voornaam: {medewerkerList[index].voornaam}\nFamilienaam: {medewerkerList[index].familienaam}\nMaandwedde: €{medewerkerList[index].maandwedde}\nStatuut: {medewerkerList[index].statuut}\nGeslacht: {medewerkerList[index].geslacht}";
+                }
+                catch
+                {
+                    MessageBox.Show("De ingevulde opslag is een ongeldige waarde.");
+                }
+            }
+        }
+
+        private void btnMin_Click(object sender, RoutedEventArgs e)//neemt geld af .... ngl kinda not cool
+        {
+            if (lsbLeden.SelectedIndex != -1)
+            {
+                try
+                {
+                    int index = lsbLeden.SelectedIndex;
+                    medewerkerList[index].minderslag(Convert.ToDouble(txbOpslag.Text));
+                    lblLidRes.Content = $"Voornaam: {medewerkerList[index].voornaam}\nFamilienaam: {medewerkerList[index].familienaam}\nMaandwedde: €{medewerkerList[index].maandwedde}\nStatuut: {medewerkerList[index].statuut}\nGeslacht: {medewerkerList[index].geslacht}";
+                }
+                catch
+                {
+                    MessageBox.Show("De ingevulde opslag is een ongeldige waarde.");
+                }
+
+            }
+
+        }
+
+        private void btnPlusOneHalf_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var medewerker in medewerkerList)//geeft iedereen 1.5% extra yeey meer taxen
+            {
+                medewerker.opslag(medewerker.maandwedde * 0.015);
+            }
+            if (lsbLeden.SelectedIndex != -1)
+            {
+                int index = lsbLeden.SelectedIndex;
+                lblLidRes.Content = $"Voornaam: {medewerkerList[index].voornaam}\nFamilienaam: {medewerkerList[index].familienaam}\nMaandwedde: €{medewerkerList[index].maandwedde}\nStatuut: {medewerkerList[index].statuut}\nGeslacht: {medewerkerList[index].geslacht}";
+            }
+        }
+
+        private void btnZoeken_Click(object sender, RoutedEventArgs e)//zoekt de naam van de persoon en geeft de eerst gevonden naam in een messagebox
+        {
+            bool search = true;
+            int i = 0;
+            do
+            {
+                if (txbZoeken.Text == medewerkerList[i].voornaam)
+                {
+                    search = false;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            while (search);
+            MessageBox.Show($"Voornaam: {medewerkerList[i].voornaam}\nFamilienaam: {medewerkerList[i].familienaam}\nMaandwedde: €{medewerkerList[i].maandwedde}\nStatuut: {medewerkerList[i].statuut}\nGeslacht: {medewerkerList[i].geslacht}");
         }
     }
 }
---
+++ LT4_OEF1/Medewerker.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LT4_OEF1
+{
+    internal class Medewerker
+    {
+        private string Voornaam { get; set; }
+        private string Familienaam { get; set; }
+        private double Maandwedde { get; set; }
+        private string Statuut { get; set; }
+        private char Geslacht { get; set; }
+
+
+
+        public Medewerker(string Voornaam, string Familienaam, double Maandwedde, string Statuut, char Geslacht)
+        {
+            this.Voornaam = Voornaam;
+            this.Familienaam = Familienaam;
+            this.Maandwedde = Maandwedde;
+            this.Statuut = Statuut;
+            this.Geslacht = Geslacht;
+        }
+        public string voornaam
+        {
+            get { return Voornaam; }
+            set { Voornaam = value; }
+        }
+        public string familienaam
+        {
+            get { return Familienaam; }
+            set { Familienaam = value; }
+        }
+        public double maandwedde
+        {
+            get { return Maandwedde; }
+            set { Maandwedde = value; }
+        }
+        public string statuut
+        {
+            get { return Statuut; }
+            set { Statuut = value; }
+        }
+        public char geslacht
+        {
+            get { return Geslacht; }
+            set { Geslacht = value; }
+        }
+        public double opslag(double opslag)
+        {
+            Maandwedde += opslag;
+            return Maandwedde;
+        }
+        public double minderslag(double opslag)
+        {
+            Maandwedde -= opslag;
+            return Maandwedde;
+        }
+    }
+}
